98105f48e9a6347b895191405eb081fd4bd3cede
Gabriel-Venancio <gabrielrvenancio.s@gmail.com>
2021-07-18 15:02:15 -0300

    change icons

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 import "./src/styles/settings/colors.css";
-import "./src/styles/generic/reset.css"
+import "./src/styles/generic/reset.css";
 import "./src/styles/elements/base.css";
 import ScoreBoard from "./src/objects/ScoreBoard";
 import BoardGame from "./src/objects/BoardGame";
-
-
 
 const $root = document.querySelector("#root");
 
@@ -15,3 +13,5 @@ $root.insertAdjacentHTML(
       ${BoardGame(2)}
     `
 );
+
+
